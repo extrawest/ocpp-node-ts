25987b5b97523b93ff9aef6fd3ed247fcf5cd83c
Igor Vorontsov <igor.vorontsov@extrawest.com>
2023-03-14 17:41:03 +0200

    Added test for OcppClientConnection's setConnection method.

--- src/OcppClientConnection.spec.ts
+++ src/OcppClientConnection.spec.ts
@@ -1,14 +1,18 @@
 import {OcppClientConnection} from "./OcppClientConnection";
 import {jest} from "@jest/globals";
+import {WS} from "jest-websocket-mock";
+import {Protocol} from "./impl/Protocol";
 
 let ocppClCon = new OcppClientConnection("testId");
+let ws: WS;
 
 describe("Test Ocpp Client Connection methods", () => {
     beforeEach(() => {
+        ws = new WS('ws://localhost:1234');
         ocppClCon = new OcppClientConnection("testId");
     });
 
-    it("test server auth", () => {
+    it("test getCpId method", () => {
         const spyGetCpId = jest.spyOn(ocppClCon, "getCpId")
         const cpId = ocppClCon.getCpId();
         expect(cpId).toBe("testId");
@@ -16,7 +20,16 @@ describe("Test Ocpp Client Connection methods", () => {
         expect(spyGetCpId).toBeCalledTimes(1)
     });
 
+    it("test setConnection method", () => {
+        const spyGetCpId = jest.spyOn(ocppClCon, "setConnection")
+        ocppClCon.setConnection(new Protocol(ocppClCon, ws as any));
+        expect(spyGetCpId).toBeCalled();
+        expect(spyGetCpId).toBeCalledTimes(1);
+        expect(spyGetCpId).toBeCalledWith(new Protocol(ocppClCon, ws as any));
+    });
+
     afterEach(() => {
         jest.clearAllMocks();
+        ws.close();
     });
 });
