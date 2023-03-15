1b3e2f36a06c63ec0de3989f5292bc25c546a386
Igor Vorontsov <igor.vorontsov@extrawest.com>
2023-03-15 15:14:16 +0200

    Covered with tests OcppServer's on method

--- src/OcppServer.spec.ts
+++ src/OcppServer.spec.ts
@@ -1,5 +1,6 @@
 import {OcppServer} from "./OcppServer";
 import {jest} from "@jest/globals";
+import {OcppClientConnection} from "./index";
 jest.mock("./impl/Server")
 
 let server: OcppServer;
@@ -21,6 +22,27 @@ describe('Test listen method functionality', () => {
         server.listen()
         expect(server.listen).toBeCalled();
         expect(server.listen).toBeCalledTimes(1);
-        expect(server.listen).toBeCalledWith(9220);
+        expect(server.listen).toBeCalledWith();
     });
 });
+
+describe('Test on method', () => {
+    beforeEach(() => {
+        server = new OcppServer();
+        jest.clearAllMocks();
+    });
+    it("test on method", () => {
+        const spy = jest.spyOn(server, "on")
+        const fakeOn = jest.fn((event: string | symbol, listener: (...args: any[]) => void) => {});
+        jest.mock('./OcppServer', () => {
+            return jest.fn().mockImplementation(() => {
+                return {on: fakeOn};
+            });
+        });
+        server.on('connection', (client: OcppClientConnection) => {})
+        expect(server.on).toBeCalled();
+        expect(server.on).toBeCalledTimes(1);
+        expect(server.on).toBeCalledWith("connection", expect.any(Function));
+    });
+});
+
